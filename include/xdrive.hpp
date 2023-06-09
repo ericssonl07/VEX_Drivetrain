58a7947b86acf5a043876e532795ba4d64f31c3d
ericssonl07 <ljh07ericsson@gmail.com>
2023-06-09 11:53:54 +0800

    Added drive control

--- include/xdrive.hpp
+++ include/xdrive.hpp
@@ -2,6 +2,7 @@
 #define DRIVETRAIN
 
 #include <vex.h>
+#include <functional>
 
 class drivetrain {
 
@@ -13,24 +14,52 @@ class drivetrain {
         vex::motor * rightMotor2;
         vex::gps * gps;
 
-        double leftMotor1Speed, leftMotor2Speed;
-        double rightMotor1Speed, rightMotor2Speed;
+        double leftMotor1Speed;
+        double leftMotor2Speed;
+        double rightMotor1Speed;
+        double rightMotor2Speed;
 
         const double distance_tolerance = 5.0;
         const double angle_tolerance = 1.0;
 
-        const double kp = 0, ki = 0, kd = 0;
-        const double kpa = 0, kia = 0, kda = 0;
+        const double kp = 0;
+        const double ki = 0;
+        const double kd = 0;
+        const double kpa = 0;
+        const double kia = 0;
+        const double kda = 0;
         double theta;
-        double sine, cosine;
-        double currentX, currentY, currentAngle; // Cartesian coordinates
-        double relativeX, relativeY, relativeAngle; // Cartesian error
-        double axis1Error, axis2Error, angleError; // Basis changed error
-        double proportionalAxis1, proportionalAxis2, proportionalAngle; // proportional
-        double integralAxis1, integralAxis2, integralAngle; // integral
-        double derivativeAxis1, derivativeAxis2, derivativeAngle; // derivative
-        double lastAxis1, lastAxis2, lastAngle; // last
-        double powerAxis1, powerAxis2, powerAngle; // sum
+        double sine;
+        double cosine;
+        double currentX;
+        double currentY;
+        double currentAngle;
+        double relativeX;
+        double relativeY;
+        double relativeAngle;
+        double axis1Error;
+        double axis2Error;
+        double angleError;
+        double proportionalAxis1;
+        double proportionalAxis2;
+        double proportionalAngle;
+        double integralAxis1;
+        double integralAxis2;
+        double integralAngle;
+        double derivativeAxis1;
+        double derivativeAxis2;
+        double derivativeAngle;
+        double lastAxis1;
+        double lastAxis2;
+        double lastAngle;
+        double powerAxis1;
+        double powerAxis2;
+        double powerAngle;
+
+        double controllerAxis1;
+        double controllerAxis2;
+        double controllerAxis3;
+        double controllerAxis4;
 
     public:
 
@@ -38,6 +67,8 @@ class drivetrain {
         void move(double x, double y, double angle);
         void turn_degrees(double angle);
         void turn_heading(double heading);
+
+        void control();
 
 };
 
--- src/xdrive.cpp
+++ src/xdrive.cpp
@@ -3,42 +3,41 @@
 #include <vex.h>
 
 drivetrain::drivetrain(vex::motor * leftMotor1, vex::motor * leftMotor2, vex::motor * rightMotor1, vex::motor * rightMotor2, vex::gps * gps) {
-    this -> leftMotor1 = leftMotor1;
-    this -> leftMotor2 = leftMotor2;
+    this -> leftMotor1  = leftMotor1;
+    this -> leftMotor2  = leftMotor2;
     this -> rightMotor1 = rightMotor1;
     this -> rightMotor2 = rightMotor2;
-    this -> gps = gps;
+    this -> gps         = gps;
 }
 
 void drivetrain::move(double x, double y, double a) {
-    const double d_t = distance_tolerance, a_t = angle_tolerance;
     integralAxis1 = integralAxis2 = integralAngle = 0;
-    currentX = gps -> xPosition();
-    currentY = gps -> yPosition();
-    currentAngle = gps -> heading();
-    relativeX = x - currentX;
-    relativeY = y - currentY;
+    currentX      = gps -> xPosition();
+    currentY      = gps -> yPosition();
+    currentAngle  = gps -> heading();
+    relativeX     = x - currentX;
+    relativeY     = y - currentY;
     relativeAngle = a - currentAngle;
-    theta = currentAngle + 45;
-    sine = sin(theta / 180.0 * M_PI);
-    cosine = cos(theta / 180.0 * M_PI);
-    lastAxis1 = cosine * currentX + sine * currentY;
-    lastAxis2 = cosine * currentY - sine * currentX;
-    angleError = relativeAngle;
-    lastAngle = angleError;
-    while (fabs(relativeX) > d_t or fabs(relativeY) > d_t or fabs(relativeAngle) > a_t) {
-        currentX = gps -> xPosition();
-        currentY = gps -> yPosition();
-        currentAngle = gps -> heading();
-        relativeX = x - currentX;
-        relativeY = y - currentY;
+    theta         = currentAngle + 45;
+    sine          = sin(theta / 180.0 * M_PI);
+    cosine        = cos(theta / 180.0 * M_PI);
+    lastAxis1     = cosine * currentX + sine * currentY;
+    lastAxis2     = cosine * currentY - sine * currentX;
+    angleError    = relativeAngle;
+    lastAngle     = angleError;
+    while (fabs(relativeX) > distance_tolerance or fabs(relativeY) > distance_tolerance or fabs(relativeAngle) > angle_tolerance) {
+        currentX      = gps -> xPosition();
+        currentY      = gps -> yPosition();
+        currentAngle  = gps -> heading();
+        relativeX     = x - currentX;
+        relativeY     = y - currentY;
         relativeAngle = a - currentAngle;
-        theta = currentAngle + 45;
-        sine = sin(theta / 180.0 * M_PI);
-        cosine = cos(theta / 180.0 * M_PI);
-        axis1Error = cosine * currentX + sine * currentY;
-        axis2Error = cosine * currentY - sine * currentX;
-        angleError = relativeAngle;
+        theta         = currentAngle + 45;
+        sine          = sin(theta / 180.0 * M_PI);
+        cosine        = cos(theta / 180.0 * M_PI);
+        axis1Error    = cosine * currentX + sine * currentY;
+        axis2Error    = cosine * currentY - sine * currentX;
+        angleError    = relativeAngle;
 
         proportionalAxis1 = axis1Error * kp;
         proportionalAxis2 = axis2Error * kp;
@@ -60,8 +59,8 @@ void drivetrain::move(double x, double y, double a) {
         powerAxis2 = proportionalAxis2 + integralAxis2 + derivativeAxis2;
         powerAngle = proportionalAngle + integralAngle + derivativeAngle;
 
-        leftMotor1Speed = powerAxis1 - powerAngle;
-        leftMotor2Speed = powerAxis2 - powerAngle;
+        leftMotor1Speed  = powerAxis1 - powerAngle;
+        leftMotor2Speed  = powerAxis2 - powerAngle;
         rightMotor1Speed = powerAxis2 + powerAngle;
         rightMotor2Speed = powerAxis1 + powerAngle;
 
@@ -78,4 +77,25 @@ void drivetrain::turn_degrees(double angle) {
 
 void drivetrain::turn_heading(double heading) {
     move(gps -> xPosition(), gps -> yPosition(), heading);
+}
+
+void drivetrain::control() {
+    vex::controller Controller(vex::controllerType::primary);
+    bool pastPrecisionState = false;
+    while (true) {
+        controllerAxis1 = Controller.Axis1.position();
+        controllerAxis2 = Controller.Axis2.position();
+        controllerAxis3 = Controller.Axis3.position();
+        controllerAxis4 = Controller.Axis4.position();
+
+        leftMotor1Speed  = controllerAxis3 + controllerAxis4 + controllerAxis1;
+        leftMotor2Speed  = controllerAxis3 - controllerAxis4 + controllerAxis1;
+        rightMotor1Speed = controllerAxis3 - controllerAxis4 - controllerAxis1;
+        rightMotor2Speed = controllerAxis3 + controllerAxis4 - controllerAxis1;
+
+        leftMotor1 -> spin(vex::directionType::fwd, leftMotor1Speed, vex::percentUnits::pct);
+        leftMotor2 -> spin(vex::directionType::fwd, leftMotor2Speed, vex::percentUnits::pct);
+        rightMotor1 -> spin(vex::directionType::fwd, rightMotor1Speed, vex::percentUnits::pct);
+        rightMotor2 -> spin(vex::directionType::fwd, rightMotor2Speed, vex::percentUnits::pct);
+    }
 }
